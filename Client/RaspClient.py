ba921af50ca80328f990864aaf87a782df0158ba
Sushanta Das <116519972+Sushanta-Das@users.noreply.github.com>
2023-11-25 10:32:41 +0530

    Update RaspClient.py

--- Client/RaspClient.py
+++ Client/RaspClient.py
@@ -6,13 +6,15 @@ import time
 # Set the DHT sensor type and GPIO pin
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4  # GPIO pin where DHT11 data pin is connected
-
 # Set the GPIO pin for the raindrop sensor
 RAIN_PIN = 17  # GPIO pin where the raindrop sensor is connected
-
 # Server endpoint to post data
-SERVER_URL = "http://127.0.0.1:5000/upload"  
+SERVER_URL = "https://weatherrpi.azurewebsites.net/upload"  
 
+def Raining(israining):
+            if (israining):
+                return "Raining"
+            return "Not Raining"
 while True:
     try:
         # Read temperature and humidity data from the DHT11 sensor
@@ -22,14 +24,17 @@ while True:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(RAIN_PIN, GPIO.IN)
         rain_data = GPIO.input(RAIN_PIN)
-
+        
+        israining= rain_data == 0 
         # Prepare the data to be sent to the server
         data_to_send = {
             "Temperature": temperature,
             "Humidity": humidity ,
-            "Rain":rain_data
+            "Rain":israining
             
         }
+        
+        print("Humidity:{0}% , Temperature:{1} , {2}".format(humidity,temperature,Raining(israining)))
 
         # Send the data to the server
         response = requests.post(SERVER_URL, json=data_to_send)
@@ -43,6 +48,4 @@ while True:
         GPIO.cleanup()
 
         time.sleep(3)  # Wait for 3 seconds before sending the next request
-
     except Exception as e:
-        print("An error occurred:", str(e))
